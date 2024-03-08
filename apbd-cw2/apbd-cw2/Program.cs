c042ce8469ef88909005ff5fad1923276b13ea06
MroziuOfficial <kubamaciekm22@gmail.com>
2024-03-08 15:17:35 +0100

    Add GetAverage method

--- apbd-cw2/apbd-cw2/Program.cs
+++ apbd-cw2/apbd-cw2/Program.cs
@@ -15,3 +15,14 @@ else
 {
     Console.WriteLine("no");
 }
+
+static double GetAverage(int[] tab)
+{
+    double result = 0.0;
+    for (int i = 0; i < tab.Length; i++)
+    {
+        result += tab[i];
+    }
+
+    return result / tab.Length;
+}
